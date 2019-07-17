a37009c7f94ef883202ff55303db4e9562837fb0
Olga Vasileva <olga.vasileva@t-systems.com>
2019-07-17 17:49:13 +0300

    [+] глава 2 итоговая задача N4

--- src/Task2_64.py
+++ src/Task2_64.py
@@ -1,14 +1,27 @@
  #[int(i) for i in input().split()]
 m = []
 while True:
-    r = [i for i in input().split()] #[input().split()]
-    if 'end' not in r:
-        for j in range(0, len(r)):
-            r[j] = int(r[j])
-        m.append(r)
-        print(m)
-    if 'end' in r:
+    n = [i for i in input().split()] #[input().split()]
+    if 'end' not in n:
+        for j in range(0, len(n)):
+            n[j] = int(n[j])
+        m.append(n)
+    if 'end' in n:
         break
+print(m) #копировать массив и писать в него
+cl = len(m)
+rw = len(n)
+fin = [[]]
+for c in range(cl,0, -1):
+    for r in range(rw, 0, -1):
+        #m[c][r]
+        for a in range(c-1, c+1, 2):
+            for b in range(r-1, r+1,2):
+                 m[c][r] += m[a][b]
+
+#элемент равен сумме соседей
+
+
 
 
     #m.append(int(r))
