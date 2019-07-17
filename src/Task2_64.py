cb4f696d825836a77532c729e6f95ee619541aef
Olga Vasileva <olga.vasileva@t-systems.com>
2019-07-17 17:18:55 +0300

    [+] глава 2 итоговая задача N4

--- src/Task2_64.py
+++ src/Task2_64.py
@@ -1,1 +1,15 @@
-m = [[int(i) for i in input().split()]]
+ #[int(i) for i in input().split()]
+m = []
+while True:
+    r = [i for i in input().split()] #[input().split()]
+    if 'end' not in r:
+        for j in range(0, len(r)):
+            r[j] = int(r[j])
+        m.append(r)
+        print(m)
+    if 'end' in r:
+        break
+
+
+    #m.append(int(r))
+
