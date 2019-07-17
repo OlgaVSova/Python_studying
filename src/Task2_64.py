ffed4c76bb3478868ffe9232fb7027bb93eb5aa5
olgasova <vassileva.os@gmail.com>
2019-07-18 00:26:48 +0300

    [+] глава 2 итоговая задача N4

--- src/Task2_64.py
+++ src/Task2_64.py
@@ -8,16 +8,19 @@ while True:
         m.append(n)
     if 'end' in n:
         break
+
 print(m) #копировать массив и писать в него
 cl = len(m)
-rw = len(n)
-fin = [[]]
-for c in range(cl,0, -1):
-    for r in range(rw, 0, -1):
-        #m[c][r]
-        for a in range(c-1, c+1, 2):
-            for b in range(r-1, r+1,2):
-                 m[c][r] += m[a][b]
+rw = len(m[:][0])
+print(cl, rw)
+fin = [[0 for i in range(0, rw)] for j in range(0, cl)]
+print(fin)
+# for c in range(cl,0, -1):
+#     for r in range(rw, 0, -1):
+#         #m[c][r]
+#         for a in range(c-1, c+1, 2):
+#             for b in range(r-1, r+1,2):
+#                  m[c][r] += m[a][b]
 
 #элемент равен сумме соседей
 
